77c7582ed6591cc75c118ab8e536bc27a924bc9d
nicopaizortiz8-ops <nicopaizortiz8@gmail.com>
2026-08-31 12:15:38 -0400

    Update brand.tsx

--- src/components/site/brand.tsx
+++ src/components/site/brand.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import logo from "@/assets/logo.png.asset.json";
+import logo from "@/assets/logo.png";
 import { cn } from "@/lib/utils";
 
 /** The supplied Yo Uso Thermomix logo. Used as provided, never redrawn. */
